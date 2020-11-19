d143c874d3587dd981f35da7c55ec9b1f108e21c
rcoconnor <33356261+rcoconnor@users.noreply.github.com>
2020-11-19 15:48:17 -0600

    Add files via upload

--- convolutional.py
+++ convolutional.py
@@ -55,16 +55,17 @@ model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), 
               metrics=['accuracy'])
 
-history = model.fit(train_images, train_labels, epochs=10, 
+history = model.fit(train_images, train_labels, epochs=20, 
                     validation_data=(test_images, test_labels))
 
+"""
 plt.plot(history.history['accuracy'], label='accuracy')
 plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
 plt.xlabel('Epoch')
 plt.ylabel('Accuracy')
 plt.ylim([0.5, 1])
 plt.legend(loc='lower right')
-
+"""
 test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
 
 print("done")
--- feedforward.py
+++ feedforward.py
@@ -31,77 +31,92 @@ def plot_images(images):
         plt.imshow(images[i].data, cmap=plt.cm.binary)
 
 
-def pre_process_images():
+def pre_process_images(filenames):
+    num_samples = 50000
     # load the data
-    images_dict = unpickle("cifar-10-batches-py/data_batch_1")
-    image_data = images_dict[b'data']
-    image_filenames = images_dict[b'filenames']
-    image_labels = images_dict[b'labels']
-    #reshape the images 
-    image_data = image_data.reshape((len(images_dict[b'data']), 3, 32, 32))
-    num_plot = 5
-    index = 0
-    f, ax = plt.subplots(num_plot, num_plot)
-    for m in range(num_plot): 
-        for n in range(num_plot): 
-            ax[m, n].imshow(image_data[index])
-            ax[m, n].get_xaxis().set_visible(False)
-            ax[m, n].get_yaxis().set_visible(False)
+    #images = np.array((num_samples, 3072), dtype='uint8')
+    #labels = np.array((num_samples,), dtype='uint8')
+    images = np.array([])
+    labels = np.array([])
+    test_images = []
+    test_labels = []
+    print("origshape: " + str(np.shape(images)))
+    for each_file in filenames:
+        file_dict = unpickle(each_file)
+        if each_file == "cifar-10-batches-py/test_batch": 
+            test_images = file_dict[b'data'] 
+            test_labels = file_dict[b'labels'] 
+        else:
+            images = np.append(images, file_dict[b'data'])
+            labels = np.append(labels, file_dict[b'labels'])
+            print("labels shape: " + str(np.shape(labels)))
+    images = images.reshape((50000, 3072))
+    print("test: " + str(np.shape(test_images)))
+    print("labe: " + str(np.shape(test_labels)))
+    return (images, labels), (test_images, test_labels)
 
-    f.subplots_adjust(hspace=0.1)
-    f.subplots_adjust(wspace=0)
-    plt.show()
-
-pre_process_images()
-#plot_images(image_data)
-
-"""
-for index in range(10): 
-    plt.subplot(5, 5, index+1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    print(dir(images))
-    print("types: " + str(type(images)))
-    plt.imshow(images[index].data, cmap=plt.cm.binary)
-"""
-#plot_images(images)
-
-exit()    
-
-# Import the fashion Mnist dataset 
-(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
-
-print("shape: " + str(np.shape(test_labels)))
-
-# convert the integers to floating point numbers 
-train_images, test_images = train_images / 255.0, test_images / 255.0
+filenames = ["cifar-10-batches-py/data_batch_1",
+             "cifar-10-batches-py/data_batch_2",
+             "cifar-10-batches-py/data_batch_3",
+             "cifar-10-batches-py/data_batch_4",
+             "cifar-10-batches-py/data_batch_5",
+             "cifar-10-batches-py/test_batch"]
 
 print("creating model....", end='')
 model = keras.models.Sequential()
-model.add(layers.Input(shape=(768,)))
-model.add(layers.Dense(768, activation="relu"))
-#model.add(layers.Dense(384, activation="relu", kernel_initializer="uniform",))
-#model.add(layers.Dense(10))
-#model.add(layers.Activation("softmax"))
+model.add(layers.Input(shape=(3072,)))
+model.add(layers.Dense(3072, activation="relu"))
+model.add(layers.Dense(1024, activation="relu"))
+model.add(layers.Dense(32, activation="relu"))
+#model.add(layers.Dense(84, activation="relu"))
+#model.add(layers.Dense(192, activation="relu"))
+#model.add(layers.Dense(96, activation="relu"))
+#model.add(layers.Dense(48, activation="relu"))
+#model.add(layers.Dense(24, activation="relu"))
+model.add(layers.Dense(12, activation="relu"))
+model.add(layers.Dense(10))
+model.add(layers.Activation("softmax"))
 print("created!")
 
-loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+(cifar_images, cifar_labels), (test_images, test_labels) = pre_process_images(filenames)
 
+# convert the integers to floating point numbers 
+cifar_images = cifar_images.astype('float32')
+test_images = test_images.astype('float32')
+cifar_images /= 255
+test_images /= 255
+
+print()
+print("train_images shape: " + str(np.shape(cifar_images)))
+print("train_labels shape: " + str(np.shape(cifar_labels)))
+print("test_images shape: " + str(np.shape(test_images)))
+print("test_labels shape: " + str(np.shape(test_labels)))
+print()
+loss_fn = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 
 model.compile(optimizer='adam',
               loss=loss_fn,
               metrics=['accuracy'])
 
 print("training model....", end='')
-history = model.fit(train_images, train_labels, epochs=10, validation_data=(test_images, test_labels))
 
+#history = model.fit(cifar_images, cifar_labels, epochs=10, validation_data=(test_images, test_labels))
+history = model.fit(cifar_images, cifar_labels, epochs=2)
+
+"""
 plt.plot(history.history['accuracy'], label='accuracy')
-plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
+#plt.plot(history.history['val_accuracy'], label = 'val_accuracy')
 plt.xlabel('Epoch')
 plt.ylabel('Accuracy')
 plt.ylim([0.5, 1])
 plt.legend(loc='lower right')
+"""
+
+print("image shape: " + str(np.shape(test_images)))
+print("label shape: " + str(np.shape(test_labels)))
+print("type: " + str(type(test_image)))
+
+print("cifar shape: " + str(cifar_images))
 
 test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
 
